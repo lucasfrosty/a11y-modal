d0c03d5a9239a2a238a1c33db28e34c7f4ff3579
lucasfrosty <lucas_frosty@hotmail.com>
2018-04-29 17:57:14 -0300

    modal working without keyboard trap!!

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
+const TAB = 9;
+
 const toggleModalButton = document.querySelector('#toggle-modal');
 const modal = document.querySelector('#modal');
 const modalContent = document.querySelector('.modal-content');
 
-toggleModalButton.addEventListener('click', () => {
-  modal.classList.toggle('hidden');
-});
+const focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
 
-modal.addEventListener('click', () => {
+let focusedElementBeforeModal;
+const focusableElements = [...modal.querySelectorAll(focusableElementsString)];
+const firstTabStop = focusableElements[0];
+const lastTabStop = focusableElements[focusableElements.length - 1];
+
+function keyDownEventListenerCallback(e) {
+  if (e.keyCode === TAB) {
+    if (e.shiftKey && document.activeElement === firstTabStop) {
+      e.preventDefault();
+      lastTabStop.focus();
+    } else if (document.activeElement === lastTabStop) {
+      e.preventDefault();
+      firstTabStop.focus();
+    }
+  }
+}
+
+function openModal() {
   modal.classList.toggle('hidden');
-});
+  focusedElementBeforeModal = document.activeElement;
+  firstTabStop.focus();
+
+  modal.addEventListener('keydown', keyDownEventListenerCallback);
+}
+
+function closeModal() {
+  focusedElementBeforeModal.focus();
+  modal.classList.toggle('hidden');
+  modal.removeEventListener('keydown', keyDownEventListenerCallback);
+}
+
+toggleModalButton.addEventListener('click', openModal); // open modal
+modal.addEventListener('click', closeModal); // close modal
 
 modalContent.addEventListener('click', (evt) => {
   evt.cancelBubble = true;
